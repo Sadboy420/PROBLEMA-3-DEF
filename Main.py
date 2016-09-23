a64704b66b4e54d53eee14a422245b62096f48cb
Sadboy420 <gabriel.gonzalezv@sansano.usm.cl>
2016-09-23 20:36:38 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+from json import loads
+
+with open('archivog enerado.json') as of:
+    data1 = loads(of.readline( ))
+
+with open('archivog enerado.json') as of:
+    data2 = loads(of.readline( ))
+
+
+
+for post in data1:
+
+for post in data2:
+    
