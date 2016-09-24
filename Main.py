8d011c3e08accac1e0431d3035bdfba58a2e85e6
Gonzalo Andres Oberreuter Alvarez <usergonzalo@hotmail.com>
2016-09-24 15:50:31 -0300

    v 0.1
    agregue listas que recopilaran datos de posts, likes y coments, y
    peronas segun mes

--- Main.py
+++ Main.py
@@ -1,16 +1,47 @@
 from json import loads
+from igraph import *
 
-with open('archivog enerado.json') as of:
+with open('beef house.json') as of: #se abren los archivos .json de ambas paginas
     data1 = loads(of.readline( ))
 
-with open('archivog enerado.json') as of:
+with open('fellini.json') as of:
     data2 = loads(of.readline( ))
 
 
+date=2015-09-24T00:00:00+0000 #fecha de restriccion(20-09-2015) a las 00:00
+posts1=[0,0,0,0,0,0,0,0,0,0,0,0]  #listas que contendran el numero de post al mes
+posts2=[0,0,0,0,0,0,0,0,0,0,0,0]
+#lista de listas que tendra los datos likes y coments por cada mes
+lkncom1=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+lkncom2=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+ppl1=[0,0,0,0,0,0,0,0,0,0,0,0]
+ppl2=[0,0,0,0,0,0,0,0,0,0,0,0]
+
 
 for post in data1:
-
+    if post["created_time"]>date:
+        for likes in post.items()[2]: #aca accede directamente a los likes
+        print likes
+    
 for post in data2:
-    
-    #holi
-    
+    if post["created_time"]>date:
+        
+
+
+
+
+
+
+
+
+
+
+
+#para el grafo    
+#g = Graph()
+#g.add_vertex(nombre_vertice)
+#g.add_vertex(nombre_vertice, tipo=tipo1)
+#g.add_edge(vertice1,vertice2)
+#g.add_edge(vertice1,vertice2,peso=2.5)
+
+
