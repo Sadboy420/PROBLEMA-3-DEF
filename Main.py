7e2d027279f4a11f3eb45f341d8a6151d25d4c3d
Sadboy420 <gabriel.gonzalezv@sansano.usm.cl>
2016-09-24 22:02:53 -0300

    Update Main.py
    lo que hice no esta muy bien pensado y tampoco tiene fecha de restriccion so feel free to ignore

--- Main.py
+++ Main.py
@@ -10,14 +10,37 @@ with open('fellini.json') as of:
 
 ##date=2015-09-20T00:00:00+0000 #fecha de restriccion(20-09-2015) a las 00:00
 
+lista1=list()
+lista2=list()
 c=0
-for post in data1:
-    for likes in post.items()[2]: #aca accede directamente a los likes
-        print likes
-    
-for post in data2:
-    pass
 
+for post in data1:
+    for likes in post.items()[2]: 
+        lista1.append(likes) #se almacenan las personas que le dan like al post en una lista
+        
+for post in data2:
+    for likes in post.items()[2]:#lo mismo de arriba
+        lista.append(likes)
+
+        
+
+print "los likes se esta pagina son",len(lista1)-1#entrega el len de la lisa-1 ya que el ultimo dato es innecesario, lo hago de esta forma porque no se me ocurre otra
+print "los likes de la segunda pagina son",len(lista2)-1
+
+
+
+
+
+
+
+
+
+#para el grafo    
+#g = Graph()
+#g.add_vertex(nombre_vertice)
+#g.add_vertex(nombre_vertice, tipo=tipo1)
+#g.add_edge(vertice1,vertice2)
+#g.add_edge(vertice1,vertice2,peso=2.5)
 
 
 
